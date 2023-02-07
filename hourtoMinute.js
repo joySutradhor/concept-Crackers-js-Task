113c5b805c912ceec97a2b246c9727f88d63a2bb
joySutradhor <joysutradhorcmt@gmail.com>
2023-02-07 19:48:10 +0600

    Convert hour to minute

---
+++ hourtoMinute.js
@@ -0,0 +1,11 @@
+
+
+function hourToMin(hour){
+    //write your code here
+    let minute = hour * 60 ;
+    return minute ;
+    //don't forget to write return
+    }
+
+    let time = hourToMin(2);
+    console.log(time);
