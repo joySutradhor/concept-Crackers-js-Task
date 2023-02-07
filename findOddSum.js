caf2a10401dee051ab59329ffcc0f566c69a002e
joySutradhor <joysutradhorcmt@gmail.com>
2023-02-07 19:06:50 +0600

    find odd numbers and return sum of add numbers

--- findOddSum.js
+++ findOddSum.js
@@ -1,18 +1,21 @@
 // Write a function findOddSum() that will take the array as the input parameter and will return the sum of the odd numbers. No need to return anything if the odd numbers do not exist in the array
 
-function findOddSum (arr) {
-    let sum = 0 ;
-for ( let i = 0; i<arr.length; i++) {
-    if ( arr [i] % 2 !== 0 ){
-        sum = sum + arr [i];
+function findOddSum(arrOfNumbers){
+    //write your code here
+          let sum = 0 ;
+    for ( let i = 0; i<arrOfNumbers.length; i++) {
+        if ( arrOfNumbers [i] % 2 !== 0 ){
+            sum = sum + arrOfNumbers [i];
+           
+        }
+
        
+    
     }
-}
-return sum
-}
-
-
-
-let numbers =[5, 7, 8, 10, 45, 30];
+    return sum
+    }
+    //don't forget to write return
+    
+let numbers =[ 8, 10, 5, 30]
 let result = findOddSum (numbers);
 console.log(result);
